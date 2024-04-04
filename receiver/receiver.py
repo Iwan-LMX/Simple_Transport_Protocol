cd14909eb67d0134b34a19d0ccb96a25e8788519
Iwan Li <iwan.li@outlook.com>
2024-04-04 15:35:19 +1100

    Base

---
+++ receiver/receiver.py
@@ -0,0 +1,3 @@
+""" 
+arguments: receiver_port; sender_port, txt_file_received, max_win
+"""
---
+++ sender/sender.py
@@ -0,0 +1,19 @@
+"""
+arguments: sender_port (49152 - 65535), receiver_port, txt_file_to_send, max_win (>= 1000B), rto, flp, rlp.
+"""
+# included head files
+
+
+#--------------------------------------------------------#
+#----------------Self defined functions------------------#
+#--------------------------------------------------------#
+# Encapsulate STP
+
+# Loss packets process 
+
+
+# 1. execute connection setup
+
+# 2. data transmission
+
+# 3. connection teardown
