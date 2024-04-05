ca598d01b67c8a71746302350083a45e97628f88
Iwan Li <iwan.li@outlook.com>
2024-04-05 22:28:40 +1100

    Finish receiver base logic

--- sender/sender.py
+++ sender/sender.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -12,18 +11,3 @@ from dataclasses import dataclass
 NUM_ARGS  = 7  # Number of command-line arguments
 BUF_SIZE  = 3  # Size of buffer for receiving messages
 MAX_SLEEP = 2  # Max seconds to sleep before sending the next message
-=======
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import random
-import socket
-import sys
-import threading
-import time
-from dataclasses import dataclass
-
-NUM_ARGS  = 7  # Number of command-line arguments
-BUF_SIZE  = 3  # Size of buffer for receiving messages
-MAX_SLEEP = 2  # Max seconds to sleep before sending the next message
->>>>>>> 56be9e0d6c8a7b59be79de63eb5e326e70542151
